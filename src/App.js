0591d756f891320534a494e347711926d7194fc3
vipul-vinsol <vipul@vinsol.com>
2019-05-13 14:42:25 +0530

    Bulma setup done and cleaned defaults of create react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class Navigation extends React.Component {
+  state = {modalOpen: false}
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="navbar-container">
+      <div className={this.state.modalOpen ? 'modal is-active' : 'modal'}>
+        <div className="modal-background"></div>
+          <div className="modal-content">
+        </div>
+        <button onClick={(e) => this.setState(prevState => {return {modalOpen: !prevState.modalOpen}})} className="modal-close is-large" aria-label="close"></button>
+      </div>
+      <div className="custom-navbar-logo">
+        BULMA
+      </div> 
+      <div className="custom-navbar">
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+          <a onClick={(e) => this.setState(prevState => {return {modalOpen: !prevState.modalOpen}})} role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <a className="navbar-item">
+              Home
+            </a>
+            <a className="navbar-item">
+              Documentation
+            </a>
+          </div>
+        </div>
+      </nav>
     </div>
-  );
+  </div>
+        )};
+      }
+      
+const App = () => {
+  return (
+    <>
+      <Navigation />
+
+    </>
+  )
 }
 
 export default App;
