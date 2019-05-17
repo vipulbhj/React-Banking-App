9e147871e9a8af3d49598083c10599bccc9fc304
vipulbhj <vipulsworkmail@gmail.com>
2019-05-17 09:19:25 +0530

    update and patching

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Cookies from 'js-cookie';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 import IndexPage from './Pages/IndexPage/IndexPage';
 import HomePage from './Pages/HomePage/HomePage';
@@ -8,11 +9,54 @@ import Navbar from './components/NavBar/Navbar';
 import './App.css';
 import './theme.css';
 
+// const fakeAuth = {
+//   isAuthenticated: false,
+//   authenticate(cb) {
+//     this.isAuthenticated = true
+//     setTimeout(cb, 100)
+//   },
+//   signout(cb) {
+//     this.isAuthenticated = false
+//     setTimeout(cb, 100)
+//   }
+// }
+
+// const PrivateRoute = ({ component: Component, ...rest }) => (
+//   <Route {...rest} render={(props) => (
+//     fakeAuth.isAuthenticated === true
+//       ? <Component {...props} />
+//       : <Redirect to={{
+//           pathname: '/login',
+//           state: { from: props.location }
+//         }} />
+//   )} />
+// )
+
+export const getSession = () => {
+  const jwt = Cookies.get('__session')
+  let session
+  try {
+    if (jwt) {
+      const base64Url = jwt.split('.')[1]
+      const base64 = base64Url.replace('-', '+').replace('_', '/')
+      session = JSON.parse(window.atob(base64))
+    }
+  } catch (error) {
+    console.log(error)
+  }
+  return session
+}
+
+export const logOut = () => {
+  Cookies.remove('__session')
+}
+
 class App extends React.Component {
   render() {
     return (
       <Router>
         <div className="app-container">
+          {console.log("Session",getSession())}
           <Navbar />
           <Switch>
             <Route exact path="/" component={IndexPage} />
--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -5,12 +5,12 @@ const FormLayout = ({ isSubmitting, animationToggle }) => (
   <div id="login" className="formContent">
     <h2 className="active"> Log In </h2>
     <Form>
-      <Field type="email" className={`${animationToggle} fadeIn-second`} 
+      <Field type="email" className={`${animationToggle} fadeIn-second`}
         name="email" placeholder="Email" />
       <ErrorMessage name="email" component="div" />
-      <Field type="password" className={`${animationToggle} fadeIn-third`} 
+      <Field type="password" className={`${animationToggle} fadeIn-third`}
         name="password" placeholder="Password" />
-      <Field type="submit" className={`${animationToggle} fadeIn-fourth`} 
+      <Field type="submit" className={`${animationToggle} fadeIn-fourth`}
         value="Log In" disabled={isSubmitting} />
     </Form>
     <div className="formFooter">
@@ -22,32 +22,48 @@ const FormLayout = ({ isSubmitting, animationToggle }) => (
 const LoginForm = (props) => {
   const animationToggle = props.inView ? 'fadeIn' : 'hidden';
   return (
-  <Formik
-    initialValues={{ email: '', password: '' }}
-    validate={values => {
-      let errors = {};
-      if (!values.email) {
-        errors.email = 'Email is Required';
-      } else if (
-        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-      ) {
-        errors.email = 'Invalid email address';
-      } else if (!values.password) {
-        errors.password = 'Password is Required';
-      } else if (values.password.length < 6) {
-        errors.password = 'Password must be more than 6 characters';
-      }
-      return errors;
-    }}
-    onSubmit={(values, { setSubmitting }) => {
-      setTimeout(() => {
-        alert(JSON.stringify(values, null, 2));
-        setSubmitting(false);
-      }, 400);
-    }}
-    render={props => <FormLayout animationToggle={animationToggle} {...props} />}
-  />
-)};
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      validate={values => {
+        let errors = {};
+        if (!values.email) {
+          errors.email = 'Email is Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        } else if (!values.password) {
+          errors.password = 'Password is Required';
+        } else if (values.password.length < 6) {
+          errors.password = 'Password must be more than 6 characters';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting, setStatus }) => {
+        fetch('https://arab-exercise-api.herokuapp.com/login', {
+          method: 'POST',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(values),
+        })
+          .then((response) => {
+            return response.json();
+          })
+          .then((myJson) => {
+            console.log(myJson);
+            console.log(JSON.stringify(myJson));
+          })
+          .catch(err => {
+            setSubmitting(false);
+            setStatus({ msg: 'Set some arbitrary status or data' });
+          });
+      }}
+      render={props => <FormLayout animationToggle={animationToggle} {...props} />}
+    />
+  )
+};
 
 
 export default LoginForm;
