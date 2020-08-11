b98d1dfcb32cd38f1ae12455e7e5fe609afccbff
Kalpesh Singh <kalpesin@amazon.com>
2020-08-11 11:00:33 +0530

    test(App.test): add test for all the routes in App.js also refactor App.js to make it testable

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,8 +1,13 @@
+/** 3P imports **/
 import React from 'react';
 import {render, fireEvent} from '@testing-library/react';
 import {Router} from 'react-router-dom';
-import {Routes} from '../App';
 import {createMemoryHistory} from 'history';
+
+/** test function imports **/
+import {Routes} from '../App';
+import * as mockAuth from "../auth";
+
 
 test('landing on a bad page shows no match component', () => {
     const history = createMemoryHistory({initialEntries: ['/something']});
@@ -15,4 +20,86 @@ test('landing on a bad page shows no match component', () => {
     fireEvent.click(getByText(/home page/i));
     expect(history.location.pathname).toBe("/");
     expect(getByText('Apply For Loan In 4 Easy Steps')).toBeInTheDocument();
+});
+
+test('landing on root page shows index page component', () => {
+    const history = createMemoryHistory({initialEntries: ['/']});
+    const {getByText, queryByText} = render(
+        <Router history={history}>
+            <Routes/>
+        </Router>
+    );
+    expect(history.location.pathname).toBe("/");
+    expect(getByText('Apply For Loan In 4 Easy Steps')).toBeInTheDocument();
+    expect(queryByText('404 - Page Not Found')).not.toBeInTheDocument();
+});
+
+test('should render home page if authenticated', () => {
+
+    jest.spyOn(mockAuth, 'authenticated').mockImplementation(() => true);
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce({});
+
+    const history = createMemoryHistory({initialEntries: ['/home']});
+
+    const {getByText, queryByText} = render(
+        <Router history={history}>
+            <Routes/>
+        </Router>
+    );
+    expect(history.location.pathname).toBe("/home");
+    expect(getByText('Choose Application Type')).toBeInTheDocument();
+    expect(queryByText('Apply For Loan In 4 Easy Steps')).not.toBeInTheDocument();
+
+    jest.spyOn(mockAuth, 'authenticated').mockRestore();
+
+});
+
+test('should render loan page if authenticated', () => {
+
+    jest.spyOn(mockAuth, 'authenticated').mockImplementation(() => true);
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce({});
+
+    const history = createMemoryHistory({initialEntries: ['/loan']});
+
+    const {getByText, queryByText} = render(
+        <Router history={history}>
+            <Routes/>
+        </Router>
+    );
+    expect(history.location.pathname).toBe("/loan");
+    expect(getByText('Loan Application Form')).toBeInTheDocument();
+    expect(queryByText('Apply For Loan In 4 Easy Steps')).not.toBeInTheDocument();
+
+    jest.spyOn(mockAuth, 'authenticated').mockRestore();
+
+});
+
+test('landing on home page without authenticated redirects to index page', () => {
+
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce({});
+
+    const history = createMemoryHistory({initialEntries: ['/home']});
+    const {getByText, queryByText} = render(
+        <Router history={history}>
+            <Routes/>
+        </Router>
+    );
+    expect(history.location.pathname).toBe("/");
+    expect(getByText('Apply For Loan In 4 Easy Steps')).toBeInTheDocument();
+    expect(queryByText('Choose Application Type')).not.toBeInTheDocument();
+});
+
+test('landing on loan page without authenticated redirects to index page', () => {
+
+    jest.spyOn(global, 'fetch').mockResolvedValueOnce({});
+
+    const history = createMemoryHistory({initialEntries: ['/loan']});
+    const {getByText, queryByText} = render(
+        <Router history={history}>
+            <Routes/>
+        </Router>
+    );
+    expect(history.location.pathname).toBe("/");
+    expect(getByText('Apply For Loan In 4 Easy Steps')).toBeInTheDocument();
+    expect(queryByText('Choose Application Type')).not.toBeInTheDocument();
 });
